69b541da1b7f6ffe1b39442fd5a0a88490f213db
ZhaoXuDongTJ <921982351@qq.com>
2017-10-04 00:20:00 +0800

    报错

--- app/src/main/java/com/maimai/zz/maimai/litepalTestActivity.java
+++ app/src/main/java/com/maimai/zz/maimai/litepalTestActivity.java
@@ -1,20 +1,12 @@
 package com.maimai.zz.maimai;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.maimai.zz.maimai.litepals.BookLibrary;
 import com.maimai.zz.maimai.utils.ImgUtils;
-
-import org.litepal.crud.DataSupport;
-
-import java.util.List;
 
 public class litepalTestActivity extends AppCompatActivity {
     //
@@ -30,7 +22,6 @@ public class litepalTestActivity extends AppCompatActivity {
     public int noNumber;
     public int TotalNumber;
     public int lastNumber;
-    private List<BookLibrary> bookLibraries;
     // imgeUtils
     private ImgUtils imgUtils;
     // litepal
@@ -48,63 +39,12 @@ public class litepalTestActivity extends AppCompatActivity {
         buttonUp = (Button) findViewById(R.id.buttonUp);
         buttonDown = (Button) findViewById(R.id.buttonDown);
         //
-        noNumber = 0;
-        bookLibraries = DataSupport.findAll(BookLibrary.class);
-        TotalNumber = bookLibraries.size();
-        lastNumber = TotalNumber-1;
-
-        //  启动  处理;
-        switch (TotalNumber){
-            case 0:
-                Toast.makeText(litepalTestActivity.this, "数据库无可书，请添加后再使用此功能", Toast.LENGTH_SHORT).show();
-                break;
-            default:
-                ShowAll(0);
-                break;
-        }
-        // 切换 数据 使用的
-        buttonUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
 
-                switch (noNumber){
-                  case 0:
-                      noNumber = TotalNumber-1;
-                      ShowAll(noNumber);
-                      break;
-                  default:
-                      noNumber--;
-                      ShowAll(noNumber);
-                      break;
-                }
-            }
-        });
 
-        buttonDown.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(noNumber == lastNumber){
-                    noNumber = 0;
-                    ShowAll(noNumber);
-                }else {
-                    noNumber++;
-                    ShowAll(noNumber);
-                }
-            }
-        });
-        //
 
     }
 
-    public void ShowAll(int num){
-        BookLibrary bookLibrary = bookLibraries.get(num);
-        ShowStudentId.setText(bookLibrary.getStudentID());
-        ShowScanNode.setText(bookLibrary.getScanCode());
-        Bitmap bitmap = ImgUtils.toImg(bookLibrary.getCover()) ;
-        ShowUserBook.setImageBitmap(bitmap);
 
-    }
 
 }
