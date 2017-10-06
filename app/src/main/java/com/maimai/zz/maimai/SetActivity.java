8e0ad0b21cd3466cc7589d7dd4a32982ddddd514
Peng Jiayao <735443617@qq.com>
2017-10-06 15:20:07 +0800

    pjy wifi setting

--- app/src/main/java/com/maimai/zz/maimai/SetActivity.java
+++ app/src/main/java/com/maimai/zz/maimai/SetActivity.java
@@ -2,21 +2,24 @@ package com.maimai.zz.maimai;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.maimai.zz.maimai.bombs.BookLibBomb;
 import com.maimai.zz.maimai.bombs.StudentInfo;
 
+import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
 import cn.bmob.v3.listener.UpdateListener;
 
 public class SetActivity extends AppCompatActivity {
-    private String xuehao;
+    private String wifi;
     private String louhao;
     private String sushehao;
     public SharedPreferences pref;
@@ -25,23 +28,26 @@ public class SetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
-        TextView sxuehao = (TextView)findViewById(R.id.textViewx);
+        // TextView sxuehao = (TextView)findViewById(R.id.textViewx);
         final EditText slou = (EditText)findViewById(R.id.editTextl);
         final EditText sshu = (EditText)findViewById(R.id.editTexts);
+        final EditText swifi = (EditText)findViewById(R.id.editTextwifi);
         pref = getSharedPreferences("data",MODE_PRIVATE);
         Button button = (Button) findViewById(R.id.buttont);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 louhao = slou.getText().toString();         //获取楼号
-                sushehao = sshu.getText().toString();      //获取宿舍号
-                if (louhao.isEmpty()==true||sushehao.isEmpty()==true){
-                    Toast.makeText(SetActivity.this,"学号，楼号，宿舍号不能为空",Toast.LENGTH_SHORT).show();
+                sushehao = sshu.getText().toString();
+                wifi = swifi.getText().toString();
+                if (louhao.isEmpty()==true||sushehao.isEmpty()==true || wifi.isEmpty()==true){
+                    Toast.makeText(SetActivity.this,"学号，楼号，wifi密码不能为空",Toast.LENGTH_SHORT).show();
                 }
                 else{
                     StudentInfo studentInfo = new StudentInfo();
                     studentInfo.setBlockNumber(Integer.valueOf(louhao));
                     studentInfo.setRoomNumber(Integer.valueOf(sushehao));
+                    studentInfo.setWifiPassword(wifi);
                     studentInfo.update(pref.getString("ObjectID",""), new UpdateListener() { /////
                         @Override
                         public void done(BmobException e) {
