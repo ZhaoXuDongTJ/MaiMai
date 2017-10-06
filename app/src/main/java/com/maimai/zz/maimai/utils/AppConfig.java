37a744272934249a6fc6f4d73928f71ff7e5c04b
ZhaoXuDongTJ <921982351@qq.com>
2017-10-06 23:23:18 +0800

    add

--- app/src/main/java/com/maimai/zz/maimai/utils/AppConfig.java
+++ app/src/main/java/com/maimai/zz/maimai/utils/AppConfig.java
@@ -11,7 +11,7 @@ public class AppConfig {
     public static String String_ModuleNumber = "    模板贡献 : ";
     public static String String_fahuo = " 待发货 : ";
     public static String String_shouhuo = "   待收货 : ";
-    public static String String_gongxiang = "   贡献书本 : ";
+    public static String String_gongxiang = "   书籍发布 : ";
     public static String String_jieyue = "   节约书本 : ";
 
 
